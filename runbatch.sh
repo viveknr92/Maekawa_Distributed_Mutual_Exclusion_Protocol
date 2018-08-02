b001aad006239a390dbda677b254276d57fe9bf0
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-08-02 13:50:33 -0500

    script updated

--- runbatch.sh
+++ runbatch.sh
@@ -8,13 +8,17 @@ for i in "${req[@]}"
 do
 	for j in "${cs[@]}"
 	do
-		read continue
+		c="${i}_$j"
 		cd $HOME/mae
-		./launcher.sh config_1_$i_$j.txt vxn170230
-		echo "done1"
-		read continue
+		./launcher.sh config_1_$c.txt vxn170230
+		read
+		echo "launcher done"
 		cd $HOME/mae
-		./cleanup.sh config_1_$i_$j.txt vxn170230
-		echo "done"
+		./cleanup.sh config_1_$c.txt vxn170230
+		read
+		echo "clean done"
     done
 done
+echo "Everything done"
+read
+echo "Everything done"
