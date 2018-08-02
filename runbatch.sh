f838dd8c2ec521ef3bf890f985469de23e6cc905
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-08-02 13:21:58 -0500

    run batch update for loop

--- runbatch.sh
+++ runbatch.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "don2"
-cd $HOME/mae
-./launcher.sh config.txt vxn170230
-echo "done1"
-read continue
-cd $HOME/mae
-./cleanup.sh config.txt vxn170230
-echo "done"
+req=( "5" "20" "50")
+
+cs=( "5" "20" "50")
+
+for i in "${req[@]}"
+do
+	for j in "${cs[@]}"
+	do
+		read continue
+		cd $HOME/mae
+		./launcher.sh config_1_$i_$j.txt vxn170230
+		echo "done1"
+		read continue
+		cd $HOME/mae
+		./cleanup.sh config_1_$i_$j.txt vxn170230
+		echo "done"
+    done
+done
