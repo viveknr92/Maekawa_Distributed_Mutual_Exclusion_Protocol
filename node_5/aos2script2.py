cb6beb089ac1bd8319a1f898cd34b51118524d4c
Patil <amp170830@utdallas.edu>
2018-08-02 19:09:38 -0500

    test data

---
+++ node_5/aos2script2.py
@@ -0,0 +1,46 @@
+# import os
+# path = r'C:\Users\Abhishek\eclipse-workspace\Maekawa_Protocol\node_5'
+# files = []
+# for i in os.listdir(path):
+#     if os.path.isfile(os.path.join(path,i)) and 'test_data_20_5' in i.startswith('test_data_'):
+#         files.append(i)
+
+# import os
+
+# prefixed = [filename for filename in os.listdir(path) if filename.contains("test_data_")]
+# print(prefixed)
+
+import fnmatch
+import os
+import csv
+import itertools
+
+arr1 = [5,20,50]
+arr2 = [5,20,50]
+comb = list(itertools.product(arr1, arr2))
+
+listOfSum = []
+
+path = r'C:\Users\Abhishek\eclipse-workspace\Maekawa_Protocol\node_5'
+for i in comb:
+    totalMsgComp = 0
+    msgComp = []
+    throughput = []
+    files = []
+    for file in os.listdir(path):
+        if fnmatch.fnmatch(file, '*_' + str(i[0]) + '_' + str(i[1]) + '.csv'):
+            files.append(file)
+
+    for file in files:
+        with open(file) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            row = list(readCSV)
+            msgComp.append(row[10])
+            throughput.append(row[11])
+
+
+    for msg in msgComp:
+        totalMsgComp += int(msg[0].split(": ", 1)[1])
+
+    listOfSum.append(totalMsgComp)
+print(listOfSum)
